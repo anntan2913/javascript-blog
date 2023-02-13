a20da15ce7a859aaa169e5f28dcff6930bf0a0f4
Anna Tankielun <anna.r.tankielun.3@gmail.com>
2023-02-13 16:53:26 +0100

    add class 'active' to the clickedElement

--- js/script.js
+++ js/script.js
@@ -5,9 +5,10 @@
     })   */
 
     const titleClickHandler = function(event){
+        const clickedElement = this;
         console.log('Link was clicked!');
       
-        /* remove class 'active' from all article links  */
+        /* [DONE] remove class 'active' from all article links  */
 
         const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -15,9 +16,12 @@
             activeLink.classList.remove('active');
         }
       
-        /* add class 'active' to the clicked link */
+        /* [IN PROGRESS] add class 'active' to the clicked link */
+        console.log('clickedElement:', clickedElement);
+
+        clickedElement.classList.add('active');
       
-        /* remove class 'active' from all articles */
+        /* [DONE] remove class 'active' from all articles */
         const activeArticles = document.querySelectorAll('.posts .active');
 
         for(let activeArticle of activeArticles){
